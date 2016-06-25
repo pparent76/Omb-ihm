cbd144730f45e364553d4cc1591f8145917d2006
Pierre Parent <pierre.parent@pparent.fr>
2016-06-25 12:59:18 +0200

    Small correction

--- make-tls-key.sh
+++ make-tls-key.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Read 32 Bytes of entropy just to make sure /dev/urandom is correctly reseeded
+#See https://lists.torproject.org/pipermail/tor-talk/2014-January/031773.html
+dd if=/dev/random bs=8 count=4
+
 mkdir /etc/ssl/
 cd /etc/ssl/
 openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
