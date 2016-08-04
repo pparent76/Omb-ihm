5d9b276331dc1f01a85ba2c1c764851f4a3f9c43
Pierre Parent <pierre.parent@pparent.fr>
2016-08-04 10:30:10 +0200

    Use perl instead of sed

--- setup-gnupg.sh
+++ setup-gnupg.sh
@@ -4,9 +4,20 @@ exec 2>&-
 
 
 cp /usr/lib/cgi-bin/gpgscript /tmp/gpgscript  >/tmp/resgpg 2>&1
-sed -i "s/MAIL/$1/g" /tmp/gpgscript
-sed -i "s'PASS'$2'g" /tmp/gpgscript
-sed -i "s/FN/$3/g" /tmp/gpgscript
+
+export VAR=$1
+perl -p -e 's/MAIL/$ENV{VAR}/g' /tmp/gpgscript > /tmp/gpgscript2
+mv /tmp/gpgscript2 /tmp/gpgscript
+
+
+export VAR=$2
+perl -p -e 's/PASS/$ENV{VAR}/g' /tmp/gpgscript > /tmp/gpgscript2
+mv /tmp/gpgscript2 /tmp/gpgscript
+
+export VAR=$3
+perl -p -e 's/FN/$ENV{VAR}/g' /tmp/gpgscript > /tmp/gpgscript2
+mv /tmp/gpgscript2 /tmp/gpgscript
+
 echo "Starting generating key">/tmp/resgpg
 gpg --gen-key --batch /tmp/gpgscript >/tmp/resgpg 2>&1
 rm /tmp/gpgscript
